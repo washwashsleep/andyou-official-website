7d00fcd8134022c2d795b252a0c86d4f3ca2e0d4
Simon Sun <blackghost0118@gmail.com>
2016-12-15 18:15:24 +0800

    加入ga, 還有驗證 site

--- web/controllers/prepare/index.js
+++ web/controllers/prepare/index.js
@@ -7,4 +7,9 @@ const pageComming = require('./page.comming');
 router.route('/')
     .get(pageComming);
 
+router.route('/google8a7dd20ce510daea.html')
+    .get((req, res, next) => {
+        return res.render('prepare/google8a7dd20ce510daea.html');
+    });
+
 module.exports = router;
